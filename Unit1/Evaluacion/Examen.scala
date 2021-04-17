b5c12edd7c40fbafa529eda6e50af7bb18d8933b
IgnacioCCM <carlos.campos@tectijuana.edu.mx>
2021-04-17 06:29:29 -0700

    Instrucciones

---
+++ Unit1/Evaluacion/Examen.scala
@@ -0,0 +1,14 @@
+/* 1. Comienza una simple sesión Spark.
+2. Cargue el archivo Netflix Stock CSV, haga que Spark infiera los tipos de datos.
+3. ¿Cuáles son los nombres de las columnas?
+4. ¿Cómo es el esquema?
+5. Imprime las primeras 5 columnas.
+6. Usa describe () para aprender sobre el DataFrame.
+7. Crea un nuevo dataframe con una columna nueva llamada “HV Ratio” que es la
+relación entre el precio de la columna “High” frente a la columna “Volume” de
+acciones negociadas por un día. (Hint: Es una operación de columnas).
+8. ¿Qué día tuvo el pico mas alto en la columna “Close”?
+9. Escribe con tus propias palabras en un comentario de tu codigo. ¿Cuál es el
+significado de la columna Cerrar “Close”?
+10. ¿Cuál es el máximo y mínimo de la columna “Volume”?
+*/
