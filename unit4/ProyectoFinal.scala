81d8a1e1f1e6348dde357dd35de15ddae830dd0d
IgnacioCCM <carlos.campos@tectijuana.edu.mx>
2021-06-26 08:24:27 -0700

    adding login and file upload

--- unit4/ProyectoFinal.scala
+++ unit4/ProyectoFinal.scala
@@ -1,3 +1,11 @@
 import org.apache.spark.ml.feature. {VectorAssembler, StringIndexer}
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j._
+
+////////////////////Inicio de sesion///////////////////////////////
+val Spark = SparkSession.builder().getOrCreate()
+
+////////////////////////Cargar archivo csv/////////////////////////
+val df = spark.read.option("header", "true").option("inferSchema","true").option("delimiter", ";")csv("/Users/admin/Documents/Github/Datos_Masivos/bank-full.csv")
+
+df.printSchema()
