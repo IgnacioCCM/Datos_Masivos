05a5693837e9dc8b685391ef7a3fc2a0f259a863
IgnacioCCM <carlos.campos@tectijuana.edu.mx>
2021-06-26 08:31:14 -0700

    Multilayer perceptron model

--- unit4/ProyectoFinal.scala
+++ unit4/ProyectoFinal.scala
@@ -131,3 +131,28 @@ println("Confusion matrix:")
 println(metrics.confusionMatrix)
 
 metrics.accuracy
+
+/////////////////////////////////////////////////////////Multilayer perceptron///////////////////////////////////////////////////////////////
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+// Split the data into train and test
+val splits = df2.randomSplit(Array(0.7, 0.3), seed = 12345)
+val train = splits(0)
+val test = splits(1)
+
+// specify layers for the neural network:
+// input layer of size 4 (features), two intermediate of size 5 and 4
+// and output of size 3 (classes)
+val layers = Array[Int](4, 5, 4, 3)
+
+// create the trainer and set its parameters
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
+val model = trainer.fit(train)
+
+// compute accuracy on the test set
+val result = model.transform(test)
+val predictionAndLabels = result.select("prediction", "label")
+val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+
+println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
