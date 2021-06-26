aa1735f37e360b4ea1da0898b172461a1a117fe0
IgnacioCCM <carlos.campos@tectijuana.edu.mx>
2021-06-26 08:26:27 -0700

    Data transformation

--- unit4/ProyectoFinal.scala
+++ unit4/ProyectoFinal.scala
@@ -9,3 +9,13 @@ val Spark = SparkSession.builder().getOrCreate()
 val df = spark.read.option("header", "true").option("inferSchema","true").option("delimiter", ";")csv("/Users/admin/Documents/Github/Datos_Masivos/bank-full.csv")
 
 df.printSchema()
+
+////////////////////////Tranformacion de los datos///////////////////////
+
+val label = new StringIndexer().setInputCol("y").setOutputCol("label")
+val labeltransform = label.fit(df).transform(df)
+
+val Features = (new VectorAssembler(). setInputCols (Array ("balance", "duration", "campaign", "previous")).setOutputCol("features"))
+val featurestransform = Features.transform(labeltransform)
+
+val df2 = featurestransform.select("features", "label")
